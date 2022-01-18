767656568259b409c3913b261a28a89afc3eb602
Rafael702 <rafaeldealmeida.192001@gmail.com>
2022-01-18 11:08:34 -0300

    Enviar dados do lead

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import react, {Component} from "react";
 import "./App.css"
+import Landing from "./components/pages/Landing";
 import Login from "./components/pages/Login";
 
 
@@ -7,7 +8,7 @@ class App extends Component {
     render(){
         return(
             <div className="App">
-                <Login/>
+                <Landing/>
             </div>
         );
     }
--- src/components/pages/Landing/index.js
+++ src/components/pages/Landing/index.js
@@ -3,21 +3,38 @@ import { Form, FormGroup, Label, Input, Button } from "reactstrap";
 import Header from "../../Header";
 
 export default class Landing extends Component {
-/*
-  save = (lead) => {
+  constructor(props) {
+    
+    super(props);
+    this.state = {
+      message: this.props.state ? this.props.state.message : "",
+    };
+  }
+
+  save = () => {
     const url = "http://localhost:8080/leads";
     let data = {
-      nome: lead.nome,
-      email: lead.email,
-      observacoes: lead.observacoes,
+      email: this.email,
+      nome: this.name,
+      descricao: this.observation,
     };
     const requestInfo = {
       method: "POST",
-      body: "",
+      body: JSON.stringify(data),
+      headers: new Headers({
+        "Content-Type": "application/json",
+      }),
     };
-    fetch(url, requestInfo);
+    fetch(url, requestInfo)
+      .then((response) => {
+        console.log(this.email, this.name, this.observation);
+        return response;
+      })
+      .catch((e) => {
+        this.setState({ message: e.message });
+        console.log(this.email, this.nome, this.observacoes);
+      });
   };
-*/
 
   render() {
     return (
@@ -27,22 +44,32 @@ export default class Landing extends Component {
         <Form>
           <FormGroup>
             <Label for="name"> Nome: </Label>
-            <Input type="text" id="name" placeholder="Informe o seu nome" />
+            <Input
+              type="text"
+              id="name"
+              onChange={(e) => (this.name = e.target.value)}
+              placeholder="Informe o seu nome"
+            />
           </FormGroup>
           <FormGroup>
             <Label for="email"> Email: </Label>
-            <Input type="text" id="email" placeholder="Informe o seu email" />
-          </FormGroup>
-          <FormGroup>
-            <Label for="observations"> Observações: </Label>
             <Input
               type="text"
-              id="observations"
+              id="email"
+              onChange={(e) => (this.email = e.target.value)}
+              placeholder="Informe o seu email"
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label for="observation"> Observações: </Label>
+            <Input
+              type="text"
+              id="observation"
+              onChange={(e) => (this.observation = e.target.value)}
               placeholder="Digite alguma observação"
             />
           </FormGroup>
-          <Button color="danger" block>
-            {" "}
+          <Button color="danger" block onClick={this.save}>
             ENVIAR
           </Button>
         </Form>
